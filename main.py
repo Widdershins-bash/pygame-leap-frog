a018c8adf9c0195ebadc405df7d62856c47aaab4
Kaden Biondich <kbiondich7@gmail.com>
2026-01-28 20:35:26 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+# Jam Theme: Flow
+
+# Ideas:
+# 1. Crossy road with water and logs only
+# 2. River tycoon: how big and clean can you make your river?
+
+
+import pygame
+
+
+def get_delta_time(clock: pygame.Clock, fps: int) -> float:
+    delta_time: float = clock.tick(fps) / 1000
+    delta_time = min(0.001, max(0.1, delta_time))
+    return delta_time
+
+
+if __name__ == "__main__":
+    pygame.init()
+
+    WIDTH: int = 800
+    HEIGHT: int = 800
+
+    screen: pygame.Surface = pygame.display.set_mode((WIDTH, HEIGHT))
+
+    fps: int = 60
+    clock: pygame.Clock = pygame.time.Clock()
+
+    running: bool = True
+
+    while running:
+
+        delta_time: float = get_delta_time(clock=clock, fps=fps)
+        screen.fill("white")
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        pygame.display.flip()
+
+    pygame.quit()
